004b15184cd1e23ba054fdd333d2a19a5a8d1886
Duong Laurent <laurentduong@MacBook-Air-de-Laurent.local>
2026-05-30 23:47:31 +0200

    d

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,10 @@
 <?php
 
+// src/Controller/HomeController.php
+
 namespace App\Controller;
 
+use App\Repository\CongeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +12,26 @@ use Symfony\Component\Routing\Attribute\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(CongeRepository $congeRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        // 1. Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+        $conges = [];
+
+        // 2. Si un utilisateur est connecté, on récupère ses congés
+        if ($user) {
+            if ($this->isGranted('ROLE_ADMIN')) {
+                // L'administrateur voit tout
+                $conges = $congeRepository->findAll();
+            } else {
+                // L'utilisateur classique ne voit que les siens
+                $conges = $congeRepository->findBy(['demandeur' => $user]);
+            }
+        }
+
+        // 3. Transmettre la variable 'conges' au template Home
+        return $this->render('home/index.html.twig', [
+            'conges' => $conges,
+        ]);
     }
 }
-
-?>
